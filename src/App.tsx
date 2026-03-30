f2e1bbf6b0a1d7e1d057519629b79183c59b3663
Sergey <zenkovich_so@mail.ru>
2026-03-31 01:27:36 +0700

    hashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 function Home() {
   const [count, setCount] = useState(0);
